6d8aa7ebf531643ee9848e1dfd203f14bacb6868
Francisco Abarca <faam1612@gmail.com>
2022-12-24 17:58:52 -0300

    Error handling

--- Server.py
+++ Server.py
@@ -5,3 +5,10 @@ from gevent.server import StreamServer
 from collections import namedtuple
 from io import BytesIO
 from socket import error as socket_error
+
+# Error handling
+class CommandError(Exception): pass
+class Disconnect(Exception): pass
+
+Error = namedtuple('Error', ('message',))
+
