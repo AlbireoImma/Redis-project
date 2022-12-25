1688c703629b90eaf933d653541316bd62940d1c
Francisco Abarca <faam1612@gmail.com>
2022-12-25 12:43:34 -0300

    Skeleton of the server

--- Server.py
+++ Server.py
@@ -12,3 +12,46 @@ class Disconnect(Exception): pass
 
 Error = namedtuple('Error', ('message',))
 
+
+class ProtocolHandler(object):
+    def handle_request(self, socket_file):
+        """ Parse a request from the client into it's component parts. """
+        pass
+
+    def write_response(self, socket_file, data):
+        """ Serialize the response data and send it to the client. """
+        pass
+
+class Server(object):
+    def __init__(self, host='127.0.0.1', port=31337, max_clients=64) -> None:
+        self._pool = Pool(max_clients)
+        self._server = StreamServer(
+            (host, port),
+            self.connection_handler,
+            spawn=self._pool
+        )
+        self._protocol = ProtocolHandler()
+        self._kv = {}
+
+    def connection_handler(self, conn, address):
+        """" Convert "conn" (a socket object) into a file-like object. """
+        socket_file = conn.makefile('rwb') # Socket to file
+        while True:
+            try:
+                data = self._protocol.handle_request(socket_file)
+            except Disconnect:
+                break
+
+            try:
+                resp = self.get_response(data)
+            except CommandError	as exc:
+                resp = Error(exc.args[0])
+
+            self._protocol.write_response(socket_file, resp)
+    
+    def get_response(self, data):
+        """ Unpack the data sent by the client, execute the command they specified, and pass back the return value """
+        pass
+
+    def run(self):
+        self._server.serve_forever()
